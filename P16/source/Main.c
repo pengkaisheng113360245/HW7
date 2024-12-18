9c87712e1fe2d4ea944631ee78b82194a11fcce6
pengkaisheng113360245 <ntut0977396578@gmail.com>
2024-12-18 23:51:00 +0800

    first

---
+++ P16/source/Main.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS  
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#define ENTER 13
+#define MAX 80
+
+int main(void)
+{
+    FILE* fptr;
+    char str[MAX], ch;
+    int i = 0;
+
+    if (fopen_s(&fptr, "output.txt", "a") != 0) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+
+    printf("請輸入字串，按ENTER鍵結束輸入:\n");
+    while ((ch = _getche()) != ENTER && i < MAX)
+        str[i++] = ch;
+
+    putc('\n', fptr);
+    fwrite(str, sizeof(char), i, fptr);
+    fclose(fptr);
+
+    printf("\n檔案附加完成!!\n");
+    system("pause");
+    return 0;
+}
